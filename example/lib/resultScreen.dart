6a90a2b93297c1dd32ba13feae8fd90eaf5f92a6
demonskp <yizhenyi@fangdd.com>
2021-06-15 15:44:23 +0800

    Update resultScreen.dart
    format

--- example/lib/resultScreen.dart
+++ example/lib/resultScreen.dart
@@ -10,7 +10,6 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Result'),
